fe3ebe234d497c298f6413a7a65c55e860aa3f22
lignitrboi <niceacl12@gmail.com>
2024-05-23 16:01:13 +0700

    Added Exercise 4

---
+++ Exercise4_Raweeroj_K.py
@@ -0,0 +1,9 @@
+FE = float(input())
+GB = float(input())
+ICS = float(input())
+CP = float(input())
+
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems", ICS)
+print("Computer Programming", CP)
