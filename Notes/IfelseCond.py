ec0433ea5e8806d44bde4239cc9422023e8f73cc
lignitrboi <niceacl12@gmail.com>
2024-05-27 23:39:54 +0700

    Added If else lect

---
+++ Notes/IfelseCond.py
@@ -0,0 +1,45 @@
+# Lect 36
+#money = 1000
+#if money > 300:
+#    print("Go to KFC")
+#else:
+#    print("Go to sleep")
+#print("Bye Boy Bye")
+
+# Lect 37
+#UsernameInput = input("Username: ")
+#PasswordInput = input("Password: ")
+#UsernameSignIn = input("Username: ")
+#PasswordSignIn = input("Password: ")
+
+#if UsernameInput == UsernameSignIn and PasswordInput == PasswordSignIn:
+#    print("Sign in")
+#else:
+#    print("Your username or password incorrect")
+
+# Lect 38
+#if False:
+#    print("First Condition")
+#elif True:
+#    print("Elif Condition")
+#else:
+#    print("Else Section")
+
+# Lect 39
+#score = int(input("Your Score is: "))
+#if score >= 80:
+#    print("Your grade is A")
+#elif score >= 75:
+#    print("Your grade is B+")
+#elif score >= 70:
+#    print("Your grade is B")
+#elif score >= 65:
+#    print("Your grade is C+")
+#elif score >= 60:
+#    print("Your grade is C")
+#elif score >= 55:
+#    print("Your grade is D+")
+#elif score >= 50:
+#    print("Your grade is D")
+#else:
+#    print("Your grade is F")
